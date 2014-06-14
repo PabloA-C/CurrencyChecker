8a59732b84ca65ddcc74261111e13cde49ef6c63
Null Fawkes <nullfawkes@gmail.com>
2014-06-14 18:08:15 +0200

    no message

--- index.php
+++ index.php
@@ -21,6 +21,13 @@
 		</noscript>
 	</head>
 	<body class="homepage">
+		<ul>
+
+			<!--<li OnClick="change('EUR');">
+			&#160&#160&#160EUR
+			</li> -->
+
+		</ul>
 
 		<!-- Header Wrapper -->
 		<div id="header-wrapper">
@@ -75,146 +82,143 @@
 					<?php
 					$xml = simplexml_load_file("data.xml");
 
-
-				
-					
 					for ($counter = 0; $counter < 3; $counter += 1) {
 
 						echo '	<div class="4u">
-								<!-- Feature -->
-								<section>
+<!-- Feature -->
+<section>
+<div  class="rate1">
+<ul> ';
 
-								<div  class="rate1">
-								<ul> ';
-					
-						$cont = 1;				
+						$cont = 1;
 						foreach ($xml->children() as $child) {
 
-							$min=0;
-							$max=31;
-							
-							
-							if($counter==1){
-								$min=30;
-								$max=61;
-							}
-							
-							if($counter==2){
-								$min=61;
-								$max=100;
-							}
-							
-							
-							if($cont>$min && $cont<$max){
-								
-								$value;
-								$currency;
-							
-								foreach ($child->children() as $child) {
-											
-									
-										
-										if($child->getName()=="title"){
-											
-											$currency =   $child ;
-											
-										}
-										 
-										 	
-										if($child->getName()=="description"){
-											
-											$value=   $child ;
-											
-										}
-										 					
-										
-									}
-								
-								$value = substr($value, 9, 8); 
-							
-								$currency = substr($currency, 0, 3); 
-	 								
-									echo '<button  class="buttonCustom">'.$value. ' ' .$currency. '</button>';		
-															
-							}
+							if ($child -> getName() != "base") {
 
-							$cont++;
+								$min = 0;
+								$max = 31;
+
+								if ($counter == 1) {
+									$min = 30;
+									$max = 61;
+								}
+
+								if ($counter == 2) {
+									$min = 61;
+									$max = 100;
+								}
+
+								if ($cont > $min && $cont < $max) {	$value;
+
+									$value = "";
+									$currency = "";
+
+									foreach ($child->children() as $child) {
+
+										if ($child -> getName() == "currency") {
+
+											$currency = $child;
+
+										}
+
+										if ($child -> getName() == "value") {
+
+											$value = $child;
+
+										}
+
+									}
+
+									$value = substr($value, 9, 8);
+
+									echo(" <form method='post' action='upvote.php'>
+
+<input type='hidden' name='value' value='$currency/EUR' >
+<input type='hidden' name='vae' value='$value' >
+<input type='submit' class='buttonCustom' value=' $value  $currency ' name='like'>
+
+");
+
+								}
+
+								$cont++;
+							}
 						}
 
 						echo '	</ul>
-								</div>
-								</section>
-								</div>';
+</div>
+</section>
+</div>';
 
 					}
 					?>
 
-				
+					<section id="save" class="container">
+						<ul class="actions">
+							<li>
+								<a href="#" class="button button-icon fa fa-file"  OnClick="save();">Export selection as PDF</a>
 
-				<ul class="actions">
-					<li>
-						<a href="#" class="button button-icon fa fa-file"  OnClick="save();">Export selection as PDF</a>
-					</li>
-				</ul>
-			</section>
+							</li>
+						</ul>
+					</section>
 
-		</div>
-
-		<div id="features-wrapper">
-
-			<!-- Features -->
-			<section id="features" class="container">
-				<header>
-					<h2>About Currency Checker</h2>
-				</header>
-
-				<div class="row">
-					<div class="4u">
-
-						<!-- Feature -->
-						<section>
-							<a href="http://regularjane.deviantart.com/art/Old-Reads-363428235" class="image image-full"><img src="images/pic01.jpg" alt="" /></a>
-							<header>
-								<h3>Application purpose</h3>
-							</header>
-							<p>
-								This is <strong>Currency Checker</strong>, a web application that provides users an easy way to check currency exchange rates. This application is an assigment for the Modern Content Management Technologies course.
-						</section>
-
-					</div>
-					<div class="4u">
-
-						<!-- Feature -->
-						<section>
-							<a href="http://regularjane.deviantart.com/art/Nutella-359114563" class="image image-full"><img src="images/pic02.jpg" alt="" /></a>
-							<header>
-								<h3>About me</h3>
-							</header>
-							<p>
-								My name is Pablo Manuel Arjonilla Cobreros, I'm an Computer Science erasmus student on Łódź, Poland.
-							</p>
-						</section>
-
-					</div>
-					<div class="4u">
-
-						<!-- Feature -->
-						<section>
-							<a href="http://regularjane.deviantart.com/art/Solo-Spring-358679786" class="image image-full"><img src="images/pic03.jpg" alt="" /></a>
-							<header>
-								<h3>Why a web application?</h3>
-							</header>
-							<p>
-								Web applications are an wasy way to reach more users across the world, and is a good way to implement the XML functionalities required for the project.
-							</p>
-						</section>
-
-					</div>
 				</div>
 
-			</section>
+				<div id="features-wrapper">
 
-		</div>
+					<!-- Features -->
+					<section id="features" class="container">
+						<header>
+							<h2>About Currency Checker</h2>
+						</header>
+
+						<div class="row">
+							<div class="4u">
+
+								<!-- Feature -->
+								<section>
+									<a href="http://regularjane.deviantart.com/art/Old-Reads-363428235" class="image image-full"><img src="images/pic01.jpg" alt="" /></a>
+									<header>
+										<h3>Application purpose</h3>
+									</header>
+									<p>
+										This is <strong>Currency Checker</strong>, a web application that provides users an easy way to check currency exchange rates. This application is an assigment for the Modern Content Management Technologies course.
+								</section>
+
+							</div>
+							<div class="4u">
+
+								<!-- Feature -->
+								<section>
+									<a href="http://regularjane.deviantart.com/art/Nutella-359114563" class="image image-full"><img src="images/pic02.jpg" alt="" /></a>
+									<header>
+										<h3>About me</h3>
+									</header>
+									<p>
+										My name is Pablo Manuel Arjonilla Cobreros, I'm an Computer Science erasmus student on Łódź, Poland.
+									</p>
+								</section>
+
+							</div>
+							<div class="4u">
+
+								<!-- Feature -->
+								<section>
+									<a href="http://regularjane.deviantart.com/art/Solo-Spring-358679786" class="image image-full"><img src="images/pic03.jpg" alt="" /></a>
+									<header>
+										<h3>Why a web application?</h3>
+									</header>
+									<p>
+										Web applications are an wasy way to reach more users across the world, and is a good way to implement the XML functionalities required for the project.
+									</p>
+								</section>
+
+							</div>
+						</div>
+
+					</section>
+
+				</div>
 
 	</body>
 </html>
